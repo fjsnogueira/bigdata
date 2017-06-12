ac98d36c09405812107b3e5192395b307953695c
Fernando Nogueira <fjsnogueira@gmail.com>
2017-06-12 12:05:46 +0100

    cleanup

--- bin/www.js
+++ bin/www.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
-var debug = require('debug')('standup');
 var app   = require('../app');
-
 var port = process.env.PORT || 3000;
-
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + server.address().port);
 });
